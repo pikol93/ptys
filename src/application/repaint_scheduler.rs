6a2cf45d3a71fe0ba5a2a435f0fccea95a18bfb2
pikol93 <pikol93@wp.pl>
2024-04-03 17:08:35 +0200

    Fixed context not being set for RepaintScheduler

--- src/application/repaint_scheduler.rs
+++ src/application/repaint_scheduler.rs
@@ -10,15 +10,17 @@ impl RepaintScheduler {
     pub async fn schedule_now(&self) {
         let context_lock = self.context.read().await;
 
-        if let Some(context) = &*context_lock {
+        if let Some(context) = context_lock.as_ref() {
             context.request_repaint();
+        } else {
+            println!("Could not request repaint.");
         };
     }
 
     pub fn set_context(&self, context: &Context) {
         {
             let context_lock = self.context.blocking_read();
-            if context_lock.is_none() {
+            if context_lock.is_some() {
                 return;
             }
         }
